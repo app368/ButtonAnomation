d75f8bfe6d0024c94f5ba41ef1d6f864dfd4672f
John Smith <app.mobile.368@gmail.com>
2020-10-01 09:05:09 -0700

    Initial Commit

---
+++ Animation-MyWorkout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animation-MyWorkout
+//
+//  Created by NextUser on 10/1/20.
+//  Copyright © 2020 Swift&Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
