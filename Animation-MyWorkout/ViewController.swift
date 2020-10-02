632c019273a921998c0ac29d8baf95bf4a89b563
John Smith <app.mobile.368@gmail.com>
2020-10-01 22:30:50 -0700

    Completed Version.

--- Animation-MyWorkout/ViewController.swift
+++ Animation-MyWorkout/ViewController.swift
@@ -6,15 +6,47 @@
 //  Copyright © 2020 Swift&Xcode. All rights reserved.
 //
 
+
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var buttonBackground: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        buttonBackground.layer.cornerRadius = 35
+        buttonBackground.clipsToBounds = true
+        buttonBackground.alpha = 0.0
+    }
+    
+    
+    @IBAction func touchedDown(_ sender: UIButton) {
+        
+        AudioServicesPlaySystemSound(SystemSoundID(1021))
+        
+        UIView.animate(withDuration: 0.35) {
+            self.buttonBackground.alpha = 0.5
+            self.buttonBackground.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+            sender.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        }
     }
 
+    
+    
+    @IBAction func touchedUpInSide(_ sender: UIButton) {
 
+        UIView.animate(withDuration: 0.35, animations: {
+            self.buttonBackground.alpha = 0.0
+            self.buttonBackground.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            sender.transform = CGAffineTransform.identity
+        }) { (_) in
+            self.buttonBackground.transform = CGAffineTransform.identity
+        }
+    }
 }
 
